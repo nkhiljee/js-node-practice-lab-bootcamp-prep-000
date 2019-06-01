fa6edf85b5245807208278688511517389c6e67c
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-01 20:44:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 
 var name = "Naush Khiljee"
-var height = "74"
+var height = "70"
 
-console.log("Name:")
+console.log(parseInt("Name:")+ 1)
 console.log(name)
 
 console.log("Height:")
